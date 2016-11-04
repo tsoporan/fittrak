9ff224681aef5c64be850c762473be50063829f7
Titus Soporan <titus@tsoporan.com>
2016-11-04 01:40:50 -0400

    add list components for home page #8

--- fittrack_client/src/components/Home.vue
+++ fittrack_client/src/components/Home.vue
@@ -1,31 +1,47 @@
 <template>
+<div class="container">
   <div class="new-workout">
-    <h2> It's {{ today }} </h2>
     <router-link to="/newworkout" class="button is-large is-primary is-outlined">New Workout</router-link>
   </div>
 
+  <StartedWorkouts></StartedWorkouts>
+  <LatestWorkouts></LatestWorkouts>
+</div>
 </template>
 
 <style lang="scss" scoped>
+$white: #ffffff;
+$lgrey: #e2e2e2;
+
+div.container {
+  margin: 2rem 1rem;
+}
 
 div.new-workout {
   text-align: center;
+  padding-bottom: 2rem;
 }
 
 div.new-workout h2 {
   padding: 2rem;
 }
-
 </style>
 
 <script>
 import moment from 'moment'
+import StartedWorkouts from './StartedWorkouts'
+import LatestWorkouts from './LatestWorkouts'
 
 export default {
   data () {
     return {
+      startedWorkouts: true,
       today: moment().format('MMMM Do, YYYY')
     }
+  },
+  components: {
+    StartedWorkouts,
+    LatestWorkouts
   }
 }
 </script>
---
+++ fittrack_client/src/components/LatestWorkouts.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="latest-workouts">
+  <h3> Latest Workouts </h3>
+  <div v-if="hasLatest">
+    Has latest
+  </div>
+  <div v-else>
+    <p> Nothing to see here. </p>
+  </div>
+</div>
+</template>
+
+<style lang="scss" scoped>
+$lgrey : #e2e2e2;
+
+div.latest-workouts {
+  padding: 1rem;
+}
+
+h3 {
+  font-size: 2rem;
+  border-bottom: 1px solid $lgrey;
+}
+</style>
+
+<script>
+export default {
+  data () {
+    return {
+      hasLatest: false,
+      loading: true
+    }
+  },
+  created () {
+    // Fetch the latest workouts
+    console.log('*** created latest workouts')
+  }
+}
+</script>
