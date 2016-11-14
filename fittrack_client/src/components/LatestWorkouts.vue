84ca2289f97542f9fec1b1c00df8163e2e309e6f
Titus Soporan <titus@tsoporan.com>
2016-11-14 01:30:26 -0500

    basic display of latest workouts

--- fittrack_client/src/components/LatestWorkouts.vue
+++ fittrack_client/src/components/LatestWorkouts.vue
@@ -2,7 +2,11 @@
 <div class="latest-workouts">
   <h3> Latest Workouts </h3>
   <div v-if="hasLatest">
-    Has latest
+    <ul>
+      <li v-for="w in latestWorkouts">
+        Workout {{ w.date_started }} - {{ w.date_ended }}
+      </li>
+    </ul>
   </div>
   <div v-else>
     <p> Nothing to see here. </p>
@@ -24,16 +28,39 @@ h3 {
 </style>
 
 <script>
+import utils from '../utils'
+
 export default {
   data () {
     return {
       hasLatest: false,
-      loading: true
+      loading: true,
+      error: ''
     }
   },
+
+  computed: {
+    latestWorkouts () {
+      return this.$store.state.workouts.latest
+    }
+  },
+
   created () {
     // Fetch the latest workouts
-    console.log('*** created latest workouts')
+    let latest = this.$store.state.workouts.latest
+
+    if (!latest.length) {
+      console.log('*** created latest workouts')
+      utils.getData('/workouts/').then((res) => {
+        this.loading = false
+        this.hasLatest = true
+        this.$store.commit('setLatestWorkouts', { latest: res.body })
+      }, (res) => {
+        console.log('failed to get data', res)
+      })
+    } else {
+      this.hasLatest = true
+    }
   }
 }
 </script>
