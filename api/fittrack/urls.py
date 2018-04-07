7fe913133ee13a43179a4965c2738a2bbb864454
Titus Soporan <titus@tsoporan.com>
2018-04-07 15:27:13 -0400

    Lint urls
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- api/fittrack/urls.py
+++ api/fittrack/urls.py
@@ -6,7 +6,8 @@ from django.views.generic import TemplateView
 from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from workouts.views import WorkoutList, WorkoutDetail, ExerciseList, ExerciseDetail
+from workouts.views import \
+    WorkoutList, WorkoutDetail, ExerciseList, ExerciseDetail
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html')),
@@ -14,7 +15,10 @@ urlpatterns = [
     url(r'^workouts/(?P<slug>\w+)/$', WorkoutDetail.as_view()),
     url(r'^exercises/$', ExerciseList.as_view()),
     url(r'^exercises/(?P<slug>\w+)/$', ExerciseDetail.as_view()),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(
+        r'^api-auth/',
+        include('rest_framework.urls', namespace='rest_framework')
+    ),
     url(r'^api-token-auth/', obtain_jwt_token),
     url(r'^api-token-verify/', verify_jwt_token),
     url(r'^rest-auth/', include('rest_auth.urls')),
