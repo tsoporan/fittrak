96c4a33ebe1c16ee2a322bdcdd0e49d302d1212e
Titus Soporan <titus@tsoporan.com>
2018-04-07 15:33:05 -0400

    Clean up project structure
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- api/workouts/models.py
+++ api/workouts/models.py
@@ -3,7 +3,7 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.conf import settings
 
-from utils.models import WorkoutBaseModel, UserBaseModel, BaseModel
+from fittrack.utils.models import WorkoutBaseModel, UserBaseModel, BaseModel
 
 import hashids
 
