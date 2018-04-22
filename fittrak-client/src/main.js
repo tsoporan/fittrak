22d921e0e8a734e59c85bd42d48016d63ce1f3ec
Titus Soporan <titus@tsoporan.com>
2018-04-22 19:22:54 -0400

    Configure apollo
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- fittrak-client/src/main.js
+++ fittrak-client/src/main.js
@@ -1,12 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import VueApollo from 'vue-apollo';
 
 Vue.config.productionTip = false
 
+// Apollo setup
+const httpLink = new HttpLink({
+  uri: 'http://localhost/graphql'
+});
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true
+})
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+});
+
+Vue.use(VueApollo)
+
 new Vue({
   router,
-  store,
+  provide: apolloProvider.provide(),
   render: h => h(App)
 }).$mount('#app')
