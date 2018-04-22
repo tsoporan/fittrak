c68cedf24a4e199c2b9de751fb72bbb7ec398937
Titus Soporan <titus@tsoporan.com>
2018-04-22 15:52:49 -0400

    Clear our project
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- fittrak/frontend/test/unit/specs/App.spec.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from '@/components/App'
-
-describe('App.vue', () => {
-  it('should render without crashing', () => {
-    const Constructor = Vue.extend(App)
-    new Constructor().$mount()
-  })
-})
