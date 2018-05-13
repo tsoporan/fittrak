c245ec05ac73123d48f065f6c1a1281d1799304e
Titus Soporan <titus@tsoporan.com>
2018-05-12 20:23:36 -0400

    Include account urls
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- fittrak/fittrak/urls.py
+++ fittrak/fittrak/urls.py
@@ -1,9 +1,10 @@
 """fittrak URL Configuration """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.conf import settings
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
 
 from graphene_django.views import GraphQLView
 
@@ -11,7 +12,10 @@ if not settings.DEBUG:
     csrf_exempt = lambda x: x
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', admin.site.urls),
     url(r'^graphql', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^$', login_required(
+        TemplateView.as_view(template_name='index.html'))
+        ),
 ]
