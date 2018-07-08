c10d2a2f46c60c23eb70aca596eca7b4429c766e
Titus Soporan <titus@tsoporan.com>
2018-07-08 19:03:37 -0400

    Split out the Nav component
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- fittrak-client/src/App.vue
+++ fittrak-client/src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/test">Test</router-link>
-    </div>
+    <Nav />
     <router-view/>
   </div>
 </template>
 
-<style lang="sass">
-@import "~bulma/bulma";
-</style>
+<script>
+import Nav from "./components/Nav";
+
+export default {
+  components: {
+    Nav
+  }
+};
+</script>
+
+<style lang="sass"></style>
---
+++ fittrak-client/src/components/Nav.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/history">History</router-link> |
+    <router-link to="/settings">Settings</router-link> |
+    <router-link to="/accounts/logout/">Sign out</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navigation"
+};
+</script>
