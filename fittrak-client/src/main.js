a5bc0cf7e41dfeebf6d6e74a9b09e27f6135c73b
Titus Soporan <titus@tsoporan.com>
2018-07-08 19:03:54 -0400

    Formatting
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- fittrak-client/src/main.js
+++ fittrak-client/src/main.js
@@ -5,29 +5,30 @@ import store from "./store";
 import "./registerServiceWorker";
 
 // Apollo
-import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import VueApollo from 'vue-apollo';
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import VueApollo from "vue-apollo";
 
 Vue.config.productionTip = false;
 
 // Apollo setup
+// TODO: Adapt for production URI
 const httpLink = new HttpLink({
-  uri: 'http://localhost:8000/graphql'
+  uri: "http://localhost:8000/graphql"
 });
 
 const apolloClient = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache(),
   connectToDevTools: true
-})
+});
 
 const apolloProvider = new VueApollo({
   defaultClient: apolloClient
 });
 
-Vue.use(VueApollo)
+Vue.use(VueApollo);
 
 new Vue({
   router,
