b69039d36a77c748f5053d62347fb19c6de03af6
Titus Soporan <titus@tsoporan.com>
2018-07-10 11:56:39 -0400

    Formatting
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- fittrak-client/src/main.js
+++ fittrak-client/src/main.js
@@ -1,48 +1,48 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import Cookies from 'js-cookie'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import Cookies from "js-cookie";
 
 // Apollo
-import { ApolloClient } from 'apollo-client'
-import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
-import VueApollo from 'vue-apollo'
+import VueApollo from "vue-apollo";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-const isDebug = process.env.NODE_ENV !== 'production'
+const isDebug = process.env.NODE_ENV !== "production";
 
 // Apollo setup
 // TODO: Adapt for production URI
 const httpLink = createHttpLink({
-  uri: 'http://localhost:8000/graphql',
-  credentials: isDebug ? 'include' : 'same-origin',
+  uri: "http://localhost:8000/graphql",
+  credentials: isDebug ? "include" : "same-origin",
   headers: {
-    'x-csrftoken': Cookies.get('csrftoken')
+    "x-csrftoken": Cookies.get("csrftoken")
   }
-})
+});
 
 const apolloClient = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache(),
   connectToDevTools: true
-})
+});
 
 const apolloProvider = new VueApollo({
   defaultClient: apolloClient
-})
+});
 
-Vue.use(VueApollo)
+Vue.use(VueApollo);
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   provide: apolloProvider.provide(),
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>"
+});
