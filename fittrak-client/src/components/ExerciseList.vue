844995e7a4a2426dac629f1b58b828cb6d89f199
Titus Soporan <titus@tsoporan.com>
2018-07-12 01:03:03 -0400

    Added ExerciseList component with data hookup
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

---
+++ fittrak-client/src/components/ExerciseList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="exercise in exercises" :key="exercise.id">
+          ID: {{ exercise.id }}
+          Name: {{ exercise.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import VIEWER_WORKOUT from "@/graphql/queries/viewerWorkout.graphql";
+
+export default {
+  name: "ExerciseList",
+  apollo: {
+    exercises: {
+      query: VIEWER_WORKOUT,
+      variables() {
+        return {
+          workoutId: this.$route.params.workoutId
+        };
+      },
+      update: data => data.viewer.workout.exercises
+    }
+  }
+};
+</script>
