39f4b372a670221dd09e02d951b32c9808bb37d4
Titus Soporan <titus@tsoporan.com>
2018-07-12 13:20:40 -0400

    Add ExerciseItem component
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

---
+++ fittrak-client/src/components/ExerciseItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <li>
+    <div>
+    ID: {{ exercise.id }}
+    </div>
+
+    <div>
+    Name: {{ exercise.name }}
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "ExerciseItem",
+  props: {
+    exercise: Object
+  }
+};
+</script>
+
+<style scoped>
+li {
+  border-bottom: 1px solid #ccc;
+  padding: 10px;
+}
+</style>
