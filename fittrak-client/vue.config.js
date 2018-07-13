1ae99fc5aad45ac948d9de06674d051460efe087
Titus Soporan <titus@tsoporan.com>
2018-07-13 18:30:14 -0400

    Restore webpack bundler and fix bundle names
    Outputs build assets for Django to use.
    
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- fittrak-client/vue.config.js
+++ fittrak-client/vue.config.js
@@ -1,5 +1,12 @@
+// FitTrak Vue Config
+
+const BundleTracker = require("webpack-bundle-tracker");
+
 module.exports = {
+  baseUrl: "/static/",
+  outputDir: "../fittrak/assets/bundles",
   configureWebpack: {
+    plugins: [new BundleTracker({ filename: "../fittrak/webpack-stats.json" })],
     resolve: {
       alias: {
         vue$: "vue/dist/vue.esm.js"
