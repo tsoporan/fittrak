1c8fe6e96118847e77d85ee011fd695ae6efbc04
Titus Soporan <titus@tsoporan.com>
2018-07-13 10:41:42 -0400

    Initial django tests, test user access
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- fittrak/users/tests.py
+++ fittrak/users/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from fittrak.schema import schema
+
+class UserTestCases(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        User.objects.create_user(
+            'Foo',
+            'foo@bar.com',
+            'FooBar'
+        )
+
+    def test_viewer_access(self):
+        client = Client()
+
+        resp = client.get('/graphql')
+
+        self.assertRedirects(resp, '/accounts/login/?next=/graphql')
+
+        loggedIn = client.login(username='Foo', password='FooBar')
+
+        self.assertTrue(loggedIn)
+
+        resp = client.get('/graphql')
+
+        self.assertEquals(resp.status_code, 400)
+        self.assertEquals(
+            resp.json()["errors"],
+            [{"message": "Must provide query string."}]
+        )
