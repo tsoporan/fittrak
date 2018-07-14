373121fed93ec943a3dce466bb54a9614ea37720
Titus Soporan <titus@tsoporan.com>
2018-07-14 17:43:38 -0400

    Added render tests for SetItem and SetList
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

---
+++ fittrak-client/src/__tests__/SetList.spec.js
@@ -0,0 +1,15 @@
+import { shallowMount } from "@vue/test-utils";
+import SetList from "../components/sets/SetList";
+
+describe("SetList.vue", () => {
+  test("SetList renders", () => {
+    const component = shallowMount(SetList, {
+      propsData: {
+        exercise: {
+          id: 1
+        }
+      }
+    });
+    expect(component.html()).toMatchSnapshot();
+  });
+});
