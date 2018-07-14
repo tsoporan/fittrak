5e2774986836d42d47dcb4fc5a0bf8d6f7336dd0
Titus Soporan <titus@tsoporan.com>
2018-07-14 16:09:08 -0400

    Expose add_set on schema
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- fittrak/fittrak/schema.py
+++ fittrak/fittrak/schema.py
@@ -5,7 +5,9 @@ import graphene
 from graphene_django.types import DjangoObjectType
 from graphene_django.debug import DjangoDebug
 
-from workouts.schema import CreateWorkout, RemoveWorkout, Query as WorkoutsQuery, AddExercise
+from workouts.schema import CreateWorkout, RemoveWorkout, Query as WorkoutsQuery, \
+    AddExercise, AddSet
+
 from users.schema import Query as UsersQuery
 
 
@@ -21,5 +23,6 @@ class RootMutation(graphene.ObjectType):
     create_workout = CreateWorkout.Field()
     remove_workout = RemoveWorkout.Field()
     add_exercise = AddExercise.Field()
+    add_set = AddSet.Field()
 
 schema = graphene.Schema(query=RootQuery, mutation=RootMutation)
