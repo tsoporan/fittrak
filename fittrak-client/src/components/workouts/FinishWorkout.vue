ecf3a52f9ce47c700ff877e3d1094606ab3ae969
Titus Soporan <titus@tsoporan.com>
2018-07-14 21:46:25 -0400

    Stub out FinishWorkout component
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

---
+++ fittrak-client/src/components/workouts/FinishWorkout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <form>
+      <button v-on:click.prevent="finishWorkout">Finish Workout</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FinishWorkout",
+  methods: {
+    finishWorkout() {
+
+    }
+  }
+};
+</script>
