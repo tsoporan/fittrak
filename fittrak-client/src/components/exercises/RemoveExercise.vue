ec8f25f631040dc2efbf3f61202df8a42ca3997a
Titus Soporan <titus@tsoporan.com>
2018-07-16 12:43:44 -0400

    Add RemoveExercise component
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

---
+++ fittrak-client/src/components/exercises/RemoveExercise.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button v-on:click.prevent="removeExercise">Remove</button>
+  </div>
+</template>
+
+<script>
+import REMOVE_EXERCISE from "@/graphql/mutations/removeExercise.graphql";
+
+export default {
+  name: "RemoveExercise",
+
+  methods: {
+    removeExercise() {
+      const { exercise } = this.$props;
+
+      this.$apollo.mutate({
+        mutation: REMOVE_EXERCISE,
+        variables: {
+          exerciseId: exercise.id
+        }
+      });
+    }
+  },
+
+  props: {
+    exercise: Object
+  }
+};
+</script>
