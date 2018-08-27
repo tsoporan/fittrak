ce4d740bada259f8d7e0ce41f91b523bc908dd5a
Titus Soporan <titus@tsoporan.com>
2018-08-27 17:13:46 -0400

    Configured Vuetify
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- fittrak-client/src/main.js
+++ fittrak-client/src/main.js
@@ -1,6 +1,46 @@
+import "@babel/polyfill";
+
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
+
+import {
+  Vuetify,
+  VApp,
+  VNavigationDrawer,
+  VFooter,
+  VList,
+  VBtn,
+  VIcon,
+  VGrid,
+  VToolbar,
+  transitions
+} from "vuetify";
+import "vuetify/src/stylus/app.styl";
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VFooter,
+    VList,
+    VBtn,
+    VIcon,
+    VGrid,
+    VToolbar,
+    transitions
+  },
+  theme: {
+    primary: "#06d6a0",
+    secondary: "#26547c",
+    accent: "#4ce0d2",
+    error: "#d62246",
+    info: "#26547c",
+    success: "#06d6a0",
+    warning: "#ffd166"
+  }
+});
+
 import App from "./App";
 import router from "./router";
 import Cookies from "js-cookie";
