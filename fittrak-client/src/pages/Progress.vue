4efb1511ad5189142f2567de18e7a8a13d211e14
Titus Soporan <titus@tsoporan.com>
2018-08-28 18:51:08 -0400

    Added Progress page stub
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

---
+++ fittrak-client/src/pages/Progress.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <h2 class="display-1">Progress</h2>
+        <v-divider />
+        <p>Nothing to see here.</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Progress"
+};
+</script>
+
+<style scoped>
+</style>
