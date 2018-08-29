e92a9a77d2e876669c7038f5661ea6869ecc80ee
Titus Soporan <titus@tsoporan.com>
2018-08-29 16:06:26 -0400

    Added Back component
    Uses router to go back 1
    
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

---
+++ fittrak-client/src/components/app/Back.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-btn @click.stop="goBack" flat color="secondary" outline>Back</v-btn>
+</template>
+
+<script>
+export default {
+  name: "Back",
+  methods: {
+    goBack() {
+      return this.$router.go(-1);
+    }
+  }
+};
+</script>
