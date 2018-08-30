fbde85bdd3b4d99656d77b79173e448ccc6d0530
Titus Soporan <titus@tsoporan.com>
2018-08-30 14:42:09 -0400

    Update snapshot tests for App
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- fittrak-client/src/__tests__/App.spec.js
+++ fittrak-client/src/__tests__/App.spec.js
@@ -3,8 +3,19 @@ import { shallowMount } from "@vue/test-utils";
 import App from "../App";
 
 describe("App.vue", () => {
-  test("App renders", () => {
-    const app = shallowMount(App);
-    expect(app.html()).toMatchSnapshot();
+  let wrp;
+
+  beforeEach(() => {
+    wrp = shallowMount(App, {
+      mocks: {
+        $apollo: {
+          loading: "false"
+        }
+      }
+    });
+  });
+
+  it("Renders", () => {
+    expect(wrp.html()).toMatchSnapshot();
   });
 });
