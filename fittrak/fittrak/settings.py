7241c1397b11b207fa89e1af879275ab8522345f
Titus Soporan <titus@tsoporan.com>
2018-09-01 22:34:24 -0400

    Adjust default settings values
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- fittrak/fittrak/settings.py
+++ fittrak/fittrak/settings.py
@@ -9,7 +9,7 @@ import sys
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DJANGO_DEBUG")
+DEBUG = os.getenv("DJANGO_DEBUG", False)
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
@@ -172,7 +172,7 @@ EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = os.getenv("EMAIL_PORT")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
+EMAIL_USE_TLS = True
 
 # Social auth config
 ACCOUNT_AUTHENTICATION_METHOD = "email"
