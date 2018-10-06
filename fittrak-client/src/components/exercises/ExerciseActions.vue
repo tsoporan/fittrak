0fe6820931cfb8f1b661c71f03977da0fca61fb2
Titus Soporan <titus@tsoporan.com>
2018-10-06 18:17:55 -0400

    Hook up addCustomExercise in dialog Save
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- fittrak-client/src/components/exercises/ExerciseActions.vue
+++ fittrak-client/src/components/exercises/ExerciseActions.vue
@@ -33,19 +33,19 @@
           <v-toolbar-title>Add Custom Exercise</v-toolbar-title>
           <v-spacer></v-spacer>
           <v-toolbar-items>
-            <v-btn dark flat @click.native="saveCustom">Save</v-btn>
+            <v-btn dark flat @click.native="addCustomExercise">Save</v-btn>
           </v-toolbar-items>
         </v-toolbar>
 
         <v-card-text>
           <v-layout wrap>
-            <v-flex>
+            <v-flex xs12>
               <v-text-field placeholder="Exercise name" v-model="customExerciseName" />
             </v-flex>
 
-            <v-flex>
+            <v-flex xs12>
               <v-autocomplete
-                v-model="customMuscleGroup"
+                v-model="customMuscleGroupName"
                 :items="muscleGroups"
                 placeholder="Select muscle group"
                 browser-autocomplete
@@ -64,7 +64,8 @@
 </template>
 
 <script>
-import AddExerciseMutation from "@/graphql/mutations/addExercises.graphql";
+import AddExercisesMutation from "@/graphql/mutations/addExercises.graphql";
+import AddCustomExerciseMutation from "@/graphql/mutations/addCustomExercise.graphql";
 import ExercisesQuery from "@/graphql/queries/exercises.graphql";
 import ExerciseTypesQuery from "@/graphql/queries/exerciseTypes.graphql";
 import MuscleGroupsQuery from "@/graphql/queries/muscleGroups.graphql";
@@ -77,7 +78,7 @@ export default {
     newExercises: [],
     exerciseTypes: [],
     muscleGroups: [],
-    customMuscleGroup: "",
+    customMuscleGroupName: "",
     customExerciseName: ""
   }),
 
@@ -93,10 +94,50 @@ export default {
   },
 
   methods: {
-    saveCustom() {
-      this.dialog = false;
-      this.customMuscleGroup = "";
-      this.customExerciseName = "";
+    addCustomExercise() {
+      const { customMuscleGroupName, customExerciseName } = this;
+      const { workout } = this.$props;
+
+      if (!customMuscleGroupName.length || !customExerciseName.length) {
+        return;
+      }
+
+      this.$apollo
+        .mutate({
+          mutation: AddCustomExerciseMutation,
+
+          variables: {
+            workoutId: workout.id,
+            exerciseName: customExerciseName,
+            muscleGroupName: customMuscleGroupName
+          },
+
+          update(store, { data }) {
+            const customExercise = data.addCustomExercise.exercise;
+
+            const result = store.readQuery({
+              query: ExercisesQuery,
+              variables: {
+                workoutId: workout.id
+              }
+            });
+
+            result.exercises = [customExercise, ...result.exercises];
+
+            store.writeQuery({
+              query: ExercisesQuery,
+              variables: {
+                workoutId: workout.id
+              },
+              data: result
+            });
+          }
+        })
+        .then(() => {
+          this.dialog = false;
+          this.customMuscleGroupName = "";
+          this.customExerciseName = "";
+        });
     },
 
     addExercises() {
@@ -108,7 +149,7 @@ export default {
 
       this.$apollo
         .mutate({
-          mutation: AddExerciseMutation,
+          mutation: AddExercisesMutation,
 
           variables: {
             workoutId: workout.id,
