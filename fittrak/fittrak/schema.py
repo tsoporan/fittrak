e9b18430f0f2b5bf035ac1e92a387baac3dc64af
Titus Soporan <titus@tsoporan.com>
2018-10-06 18:19:51 -0400

    Expose addCustomExercise mutation
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- fittrak/fittrak/schema.py
+++ fittrak/fittrak/schema.py
@@ -2,9 +2,9 @@
 
 import graphene
 from graphene_django.debug import DjangoDebug
-
 from users.schema import Query as UsersQuery
-from workouts.graphql.exercises import AddExercises, RemoveExercise
+from workouts.graphql.exercises import (AddCustomExercise, AddExercises,
+                                        RemoveExercise)
 from workouts.graphql.schema import Query as WorkoutsQuery
 from workouts.graphql.sets import AddSet, RemoveSet, UpdateSet
 from workouts.graphql.workouts import (CreateWorkout, RemoveWorkout,
@@ -20,6 +20,7 @@ class RootMutation(graphene.ObjectType):
     remove_workout = RemoveWorkout.Field()
     update_workout = UpdateWorkout.Field()
     add_exercises = AddExercises.Field()
+    add_custom_exercise = AddCustomExercise.Field()
     remove_exercise = RemoveExercise.Field()
     add_set = AddSet.Field()
     remove_set = RemoveSet.Field()
