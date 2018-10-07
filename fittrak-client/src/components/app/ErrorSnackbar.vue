941cfa60d40fb0033891515c922a0f5f8a2f90e9
Titus Soporan <titus@tsoporan.com>
2018-10-07 18:19:50 -0400

    Add ErrorSnackbar component
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

---
+++ fittrak-client/src/components/app/ErrorSnackbar.vue
@@ -0,0 +1,35 @@
+<template>
+<v-snackbar
+  v-model="open"
+  :timeout="timeout"
+  :bottom="bottom"
+  :color="color"
+>
+  {{ text }}
+  <v-btn
+    flat
+    @click="open = false"
+  >
+  Close
+  </v-btn>
+</v-snackbar>
+</template>
+
+<script>
+export default {
+  name: "ErrorSnackbar",
+
+  data() {
+    return {
+      open: true,
+      bottom: true,
+      timeout: 7000,
+      color: "error"
+    };
+  },
+
+  props: {
+    text: String
+  }
+};
+</script>
