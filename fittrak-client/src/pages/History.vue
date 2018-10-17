fcb18a97618f99763c01346ad492553da82cb4e5
Titus Soporan <titus@tsoporan.com>
2018-10-16 22:21:59 -0400

    Remove redundant page titles
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- fittrak-client/src/pages/History.vue
+++ fittrak-client/src/pages/History.vue
@@ -2,8 +2,6 @@
   <v-container>
     <v-layout row wrap>
       <v-flex xs12>
-        <h2 class="display-1">History</h2>
-        <v-divider />
         <WorkoutHistoryToolbar />
         <WorkoutList />
       </v-flex>
