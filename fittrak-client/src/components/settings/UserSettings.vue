173dc52c4b8172b7162167b56f1e995f469ba540
Titus Soporan <titus@tsoporan.com>
2019-01-06 20:34:08 -0500

    Settings form
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- fittrak-client/src/components/settings/UserSettings.vue
+++ fittrak-client/src/components/settings/UserSettings.vue
@@ -1,15 +1,63 @@
 <template>
   <v-flex xs12>
-    {{ user.id }}
-    {{ user.email }}
-    {{ user.username }}
-    {{ user.profile.preferredUnit }}
+    <v-form
+      ref="form" 
+      v-model="valid"
+      lazy-validation
+    >
+
+      <v-text-field
+        v-model="user.username"
+        label="Username"
+      />
+
+      <v-text-field
+        v-model="user.profile.height"
+        label="Height"
+        type="number"
+      />
+
+      <v-text-field
+        v-model="user.profile.weight"
+        label="Weight"
+        type="number"
+      />
+
+      <v-radio-group
+        v-model="user.profile.preferredUnit"
+        label="Preferred Unit"
+      >
+        <v-radio 
+          label="LBS" 
+          value="LBS"/>
+        <v-radio 
+          label="KGS" 
+          value="KGS"/>
+      </v-radio-group>
+
+      <v-divider />
+
+      <v-flex text-xs-right>
+        <v-btn 
+          flat
+          outline
+        >Submit</v-btn>
+      </v-flex>
+    </v-form>
   </v-flex>
 </template>
 
 <script>
 export default {
   name: "UserSettings",
+
+  data: function() {
+    return {
+      valid: true
+    };
+  },
+
+  methods: {},
 
   props: {
     user: {
