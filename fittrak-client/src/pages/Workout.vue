43480822bce71ec1912c5fcf43990dd9f232b4a8
Titus Soporan <titus@tsoporan.com>
2019-01-14 21:16:38 -0500

    Update workout page data fetching
    Updated queries/mutations for workouts and exercises
    
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- fittrak-client/src/pages/Workout.vue
+++ fittrak-client/src/pages/Workout.vue
@@ -1,17 +1,44 @@
 <template>
-  <v-container>
-    <WorkoutDataContainer :workout-id="$route.params.workoutId">
-      <template slot-scope="slotProps">
-        <WorkoutHeader :workout="slotProps.workout" />
-        <ExerciseActions :workout="slotProps.workout" />
-        <ExerciseList :exercises="slotProps.exercises" />
-      </template>
-    </WorkoutDataContainer>
+  <v-container
+    fill-height
+    v-if="$apollo.loading"
+  >
+    <v-layout 
+      row
+      wrap
+      align-center
+      justify-center
+    >
+      <v-flex text-xs-center>
+        <Loader 
+          size="48"
+          color="primary"
+        />
+        <span class="headline">
+          Preparing workout ...
+        </span>
+      </v-flex>
+    </v-layout>
+  </v-container>
+  <v-container
+    row 
+    wrap
+    v-else
+  >
+    <v-layout 
+      row 
+      wrap>
+      <WorkoutHeader :workout="workout" />
+      <ExerciseActions :workout="workout" />
+      <ExerciseList :exercises="exercises" />
+    </v-layout>
   </v-container>
 </template>
 
 <script>
-import WorkoutDataContainer from "@/components/workouts/WorkoutDataContainer";
+import { queries } from "@/graphql";
+
+import Loader from "@/components/app/Loader";
 import WorkoutHeader from "@/components/workouts/WorkoutHeader";
 import ExerciseActions from "@/components/exercises/ExerciseActions";
 import ExerciseList from "@/components/exercises/ExerciseList";
@@ -19,14 +46,44 @@ import ExerciseList from "@/components/exercises/ExerciseList";
 export default {
   name: "Workout",
 
+  data() {
+    return {
+      workout: {},
+      exercises: []
+    };
+  },
+
+  apollo: {
+    workout: {
+      query: queries.workoutQuery,
+      variables() {
+        const workoutId = this.$route.params.workoutId;
+
+        return {
+          workoutId
+        };
+      },
+      update: data => data.workout
+    },
+
+    exercises: {
+      query: queries.exercisesQuery,
+      variables() {
+        const workoutId = this.$route.params.workoutId;
+
+        return {
+          workoutId
+        };
+      },
+      update: data => data.exercises
+    }
+  },
+
   components: {
-    WorkoutDataContainer,
     WorkoutHeader,
     ExerciseActions,
-    ExerciseList
+    ExerciseList,
+    Loader
   }
 };
 </script>
-
-<style>
-</style>
