1d2972a731fc13346fa8366dea4c995a37db2cb4
Titus Soporan <titus@tsoporan.com>
2019-01-14 20:48:30 -0500

    Update Home data fetching
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- fittrak-client/src/pages/Home.vue
+++ fittrak-client/src/pages/Home.vue
@@ -1,119 +1,64 @@
 <template>
   <v-container 
     fluid 
-    fill-height 
-    grid-list-xl>
+    grid-list-xl
+  >
     <v-layout 
-      row 
-      align-center>
-      <v-flex 
-        xs12 
-        text-xs-center>
-        <h2 class="display-2">
-          It's {{ now.dayTime }} on {{ now.dayName }} the {{ now.dayNum }}.
-          <br >
-        </h2>
-
-        <v-flex />
-
-        <v-layout 
-          v-bind="layout" 
-          align-center>
-          <CreateWorkout />
-          <v-flex 
-            xs1 
-            text-xs-center>
-            <v-flex>OR</v-flex>
-          </v-flex>
-          <v-flex 
-            xs6 
-            text-xs-left>
-            <v-btn 
-              large 
-              disabled 
-              @click.stop="" 
-              class="success">
-              Start Routine
-            </v-btn>
-          </v-flex>
-        </v-layout>
-
-        <v-layout>
-          <v-flex xs-8>
-            <WorkoutList 
-              :limit="limit" 
-              title="Recently" />
-          </v-flex>
-        </v-layout>
-
+      row
+    >
+      <v-flex>
+        <WorkoutFilterHeader />
+      </v-flex>
+    </v-layout>
+    <v-layout fill-height>
+      <v-flex v-if="$apollo.loading">
+        <Loader 
+          color="primary" 
+          size="48" />
+      </v-flex>
+      <v-flex v-else>
+        <WorkoutCardList :workouts="workouts" />
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-import CreateWorkout from "@/components/workouts/CreateWorkout";
-import WorkoutList from "@/components/workouts/WorkoutList";
+import { queries } from "@/graphql";
 
-import { PENDING, DEFAULT_LIMIT } from "@/constants";
+import Loader from "@/components/app/Loader";
+import WorkoutFilterHeader from "@/components/workouts/WorkoutFilterHeader";
+import WorkoutCardList from "@/components/workouts/WorkoutCardList";
 
-import { format } from "date-fns";
+import { showSnackbar } from "@/helpers";
 
 export default {
   name: "Home",
 
   data() {
     return {
-      date: new Date(),
-      limit: DEFAULT_LIMIT
+      workouts: []
     };
   },
 
-  computed: {
-    now: {
-      get() {
-        const date = this.date;
+  apollo: {
+    workouts: {
+      query: queries.workoutsQuery,
 
-        if ("dayPart" in date) return date;
-
-        const dayName = format(date, "dddd");
-        const dayNum = format(date, "Do");
-        const dayTime = format(date, "h[:]mmA");
-
-        return {
-          dayName,
-          dayNum,
-          dayTime
-        };
+      update(data) {
+        return data.workouts;
       },
 
-      set(newDate) {
-        this.date = newDate;
+      error() {
+        showSnackbar("error", "Could not load workouts.");
       }
-    },
-
-    status() {
-      return PENDING;
-    },
-
-    layout() {
-      // Allow dynamic layout opts based on screen size
-      const layout = {};
-
-      if (this.$vuetify.breakpoint.xs) {
-        layout.column = true;
-      }
-
-      return layout;
     }
   },
 
   components: {
-    CreateWorkout,
-    WorkoutList
+    WorkoutFilterHeader,
+    WorkoutCardList,
+    Loader
   }
 };
 </script>
-
-<style scoped>
-</style>
