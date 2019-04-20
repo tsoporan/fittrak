45d5847a64de269a8f9abc74de17104a49e56d1c
Titus Soporan <titus@tsoporan.com>
2019-04-20 11:05:59 -0400

    contrib.messages required for django admin
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- fittrak/fittrak/settings.py
+++ fittrak/fittrak/settings.py
@@ -24,6 +24,7 @@ AUTHENTICATION_BACKENDS = (
 
 # Application definition
 INSTALLED_APPS = [
+    "django.contrib.messages",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
