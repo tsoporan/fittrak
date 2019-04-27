94a81042ff1fd276432dd5873292489a1c449c7c
Titus Soporan <titus@tsoporan.com>
2019-04-26 22:12:13 -0400

    Update bottom nav names and icons
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- fittrak-react/src/components/Navigation.js
+++ fittrak-react/src/components/Navigation.js
@@ -1,24 +1,47 @@
 import React from "react";
+
+import { withStyles } from "@material-ui/core/styles";
+
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 
-import { withStyles } from "@material-ui/core/styles";
+import ShowChart from "@material-ui/icons/ShowChart";
+import History from "@material-ui/icons/History";
+import Settings from "@material-ui/icons/Settings";
+import List from "@material-ui/icons/List";
 
 const styles = {
   root: {}
 };
 
-const $AppBottomNavigation = props => {
-  const { classes } = props;
+class $AppBottomNavigation extends React.Component {
+  state = {
+    value: 0
+  };
 
-  return (
-    <BottomNavigation className={classes.root} showLabels>
-      <BottomNavigationAction label="New" />
-      <BottomNavigationAction label="Test" />
-      <BottomNavigationAction label="Test2" />
-    </BottomNavigation>
-  );
-};
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <BottomNavigation
+        className={classes.root}
+        showLabels
+        value={value}
+        onChange={this.handleChange}
+      >
+        <BottomNavigationAction label="Workouts" icon={<List />} />
+        <BottomNavigationAction label="Progress" icon={<ShowChart />} />
+        <BottomNavigationAction label="History" icon={<History />} />
+        <BottomNavigationAction label="Settings" icon={<Settings />} />
+      </BottomNavigation>
+    );
+  }
+}
 
 const AppBottomNavigation = withStyles(styles)($AppBottomNavigation);
 
