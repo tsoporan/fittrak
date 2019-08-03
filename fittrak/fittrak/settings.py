eae4fc74b65106c4e16af10878bf1111c4aeb34f
Titus Soporan <titus@tsoporan.com>
2019-08-03 11:43:45 -0400

    Add env and release tracking to Sentry
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- fittrak/fittrak/settings.py
+++ fittrak/fittrak/settings.py
@@ -8,7 +8,12 @@ import sys
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
-sentry_sdk.init(dsn=os.getenv("SENTRY_DSN"), integrations=[DjangoIntegration()])
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DSN"),
+    integrations=[DjangoIntegration()],
+    environment=os.getenv("SENTRY_ENV"),
+    release=os.getenv("SENTRY_RELEASE"),
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
