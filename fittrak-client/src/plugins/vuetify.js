7a1c948dafbd859f5356e467875b65d6e997ccbe
Titus Soporan <titus@tsoporan.com>
2019-08-04 23:05:37 -0400

    Restore theme
    Signed-off-by: Titus Soporan <titus@tsoporan.com>

--- fittrak-client/src/plugins/vuetify.js
+++ fittrak-client/src/plugins/vuetify.js
@@ -8,15 +8,19 @@ export default new Vuetify({
     iconfont: "mdi"
   },
   theme: {
-    primary: "#06d6a0",
-    primaryDark: "#00ae80",
-    secondary: "#26547c",
-    accent: "#4ce0d2",
-    error: "#d62246",
-    info: "#26547c",
-    success: "#06d6a0",
-    warning: "#ffd166",
-    mainBg: "#fafafa",
-    darkGrey: "#424242"
+    themes: {
+      light: {
+        primary: "#06d6a0",
+        secondary: "#26547c",
+        accent: "#4ce0d2",
+        error: "#d62246",
+        info: "#26547c",
+        success: "#06d6a0",
+        warning: "#ffd166",
+        mainBg: "#fafafa",
+        primaryDark: "#00ae80",
+        darkGrey: "#424242"
+      }
+    }
   }
 });
